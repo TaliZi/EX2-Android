440816d5e11fcd60ed8584f0413db7120876375c
TaliZi <talizi1234@gmail.com>
2024-02-22 19:07:29 +0200

    edit message activity

--- app/src/main/java/com/example/taliSocialMedia/AddCommentActivity.java
+++ app/src/main/java/com/example/taliSocialMedia/AddCommentActivity.java
@@ -1,6 +1,5 @@
 package com.example.taliSocialMedia;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -16,6 +15,9 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
+/*
+       We come here from PostActivity to create a new comment.
+*/
 public class AddCommentActivity extends AppCompatActivity {
 
     @Override
@@ -23,17 +25,13 @@ public class AddCommentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_comment);
 
-
-        Button backButton,addCommentBTN;
+        // Declare UI elements
+        Button backButton, addCommentBTN;
         EditText userMessage;
         ImageView userImage;
         TextView userFirstName;
 
-        /*
-        We come here from PostActivity to create a new comment.
-         */
-
-
+        // Initialize UI elements
         backButton = findViewById(R.id.goBackBTN);
         userMessage = findViewById(R.id.usermessageET);
         userMessage.setText("");
@@ -41,46 +39,42 @@ public class AddCommentActivity extends AppCompatActivity {
         userFirstName = findViewById(R.id.username_TV);
         addCommentBTN = findViewById(R.id.finishBTN);
 
+        // Retrieve extras passed from previous activity
         Bundle extras = getIntent().getExtras();
 
+        // Set user image and first name
         userImage.setImageURI(getIntent().getData());
         userFirstName.setText(extras.getString("firstname"));
 
-
-
-
-
-
-
-
-
+        // Button click listener for adding comment
         addCommentBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Create intent to navigate back to PostActivity
                 Intent myIntent = new Intent(AddCommentActivity.this, PostActivity.class);
 
+                // Get current date and format it
                 Date c = Calendar.getInstance().getTime();
                 SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
                 String formattedDate = df.format(c);
 
+                // Attach data to the intent
                 myIntent.setData(getIntent().getData());
                 myIntent.putExtra("firstname", extras.getString("firstname"));
                 myIntent.putExtra("indicator", "afterComment");
                 myIntent.putExtra("email", extras.getString("email"));
-                myIntent.putExtra("message", userMessage.getText().toString() );
-                myIntent.putExtra("date", formattedDate );
-                startActivity(myIntent);
-
+                myIntent.putExtra("message", userMessage.getText().toString());
+                myIntent.putExtra("date", formattedDate);
+                startActivity(myIntent); // Start PostActivity
             }
         });
 
-
+        // Button click listener for going back
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onBackPressed();
+                onBackPressed(); // Go back to previous screen
             }
         });
-
     }
 }
