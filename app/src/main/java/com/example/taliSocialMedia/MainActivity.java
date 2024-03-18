2b60b05a33a0bc8b56dc323da4e9683a0d7fa2bc
TaliZi <talizi1234@gmail.com>
2024-03-18 15:52:56 +0200

    commit

--- app/src/main/java/com/example/taliSocialMedia/MainActivity.java
+++ app/src/main/java/com/example/taliSocialMedia/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.taliSocialMedia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -11,12 +13,45 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.taliSocialMedia.network.LoginResponseResult;
+import com.example.taliSocialMedia.network.User;
+import com.example.taliSocialMedia.repository.UserRepository;
+import com.example.taliSocialMedia.signin.UserViewModel;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private UserViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(UserViewModel.class);
+
+        String userId = UserRepository.getUserId(this);
+        if (userId != null) {
+            Intent myIntent = new Intent(MainActivity.this, PostActivity.class);
+
+            // Get Uri from Intent
+            Uri userImage = getIntent().getData();
+            myIntent.setData(userImage);
+
+            // Add flags to the intent
+            myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK & (Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION));
+
+            String email = UserRepository.getEmail(MainActivity.this);
+            // Put extras to the intent
+            myIntent.putExtra("firstname", email);
+            myIntent.putExtra("email", email);
+            myIntent.putExtra("indicator", "main");
+
+            // Start the activity
+            startActivity(myIntent);
+            finish();
+        }
 
         // Initialize UI elements
         TextView Title, gotoRegister;
@@ -42,43 +77,39 @@ public class MainActivity extends AppCompatActivity {
         enterBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Get extras from the intent
-                Bundle extras = getIntent().getExtras();
-                if (extras != null) {
-                    // Retrieve stored email, password, and firstname
-                    String storedpassword, storedemail, firstname, userInputEmail, userInputPassword;
-                    storedemail = extras.getString("email");
-                    storedpassword = extras.getString("password");
-                    firstname = extras.getString("firstname");
+                // Retrieve stored email, password, and firstname
+                String userInputEmail, userInputPassword;
 
-                    // Get user input email and password
-                    userInputEmail = emailET.getText().toString();
-                    userInputPassword = passwordET.getText().toString();
+                // Get user input email and password
+                userInputEmail = emailET.getText().toString();
+                userInputPassword = passwordET.getText().toString();
 
-                    // Check if the entered email and password match the stored ones
-                    if (storedemail.equals(userInputEmail) && storedpassword.equals(userInputPassword)) {
-                        // If matched, create intent to start PostActivity
-                        Intent myIntent = new Intent(MainActivity.this, PostActivity.class);
+                viewModel.login(userInputEmail, userInputPassword).observe(MainActivity.this, new Observer<LoginResponseResult>() {
+                    @Override
+                    public void onChanged(LoginResponseResult result) {
+                        if (result.isSuccess()) {
+                            // If matched, create intent to start PostActivity
+                            Intent myIntent = new Intent(MainActivity.this, PostActivity.class);
 
-                        // Get Uri from Intent
-                        Uri userImage = getIntent().getData();
-                        myIntent.setData(userImage);
+                            // Get Uri from Intent
+                            Uri userImage = getIntent().getData();
+                            myIntent.setData(userImage);
 
-                        // Add flags to the intent
-                        myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK & (Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION));
+                            // Add flags to the intent
+                            myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK & (Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION));
 
-                        // Put extras to the intent
-                        myIntent.putExtra("firstname", firstname);
-                        myIntent.putExtra("email", emailET.getText().toString());
-                        myIntent.putExtra("indicator", "main");
+                            // Put extras to the intent
+                            myIntent.putExtra("firstname", emailET.getText().toString());
+                            myIntent.putExtra("email", emailET.getText().toString());
+                            myIntent.putExtra("indicator", "main");
 
-                        // Start the activity
-                        startActivity(myIntent);
-                    } else {
-                        // If not matched, show a toast message
-                        Toast.makeText(MainActivity.this, "Password or email doesn't match", Toast.LENGTH_SHORT).show();
+                            // Start the activity
+                            startActivity(myIntent);
+                        } else {
+                            Toast.makeText(MainActivity.this, result.getErrorMessage(), Toast.LENGTH_SHORT).show();
+                        }
                     }
-                }
+                });
             }
         });
     }
