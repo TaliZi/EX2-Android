cd2d2062be97b0bd3c8040610b522bf51eaead42
TaliZi <talizi1234@gmail.com>
2024-03-18 15:59:16 +0200

    update the app - server connection

---
+++ app/src/main/java/com/example/taliSocialMedia/friends/UsersAdapter.java
@@ -0,0 +1,145 @@
+package com.example.taliSocialMedia.friends;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.taliSocialMedia.Helper;
+import com.example.taliSocialMedia.R;
+import com.example.taliSocialMedia.network.User;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder> {
+
+    private List<User> users;
+    private User currentUser;
+    private OnItemClickListener listener;
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    public UsersAdapter(List<User> users) {
+        this.users = users;
+    }
+
+    public void setUsers(List<User> newUsers, User currentUser) {
+        this.currentUser = currentUser;
+        // Calculate the differences between old and new lists
+        DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(new DiffUtil.Callback() {
+            @Override
+            public int getOldListSize() {
+                return users.size();
+            }
+
+            @Override
+            public int getNewListSize() {
+                return newUsers.size();
+            }
+
+            @Override
+            public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+                return users.get(oldItemPosition).getName().equals(newUsers.get(newItemPosition).getName());
+            }
+
+            @Override
+            public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+                return users.get(oldItemPosition).equals(newUsers.get(newItemPosition));
+            }
+        });
+
+        this.users = newUsers;
+        diffResult.dispatchUpdatesTo(this);
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.userNameTextView.setText(user.getName());
+        holder.friendButton.setEnabled(true);
+        if (currentUser.getFriends().contains(user.getId())) {
+            holder.friendButton.setImageResource(R.drawable.baseline_delete_outline_24);
+        } else if (currentUser.getFriendRequestsReceived().contains(user.getId())) {
+            holder.friendButton.setImageResource(R.drawable.baseline_check_24);
+        } else if (currentUser.getFriendRequestsSent().contains(user.getId())) {
+            holder.friendButton.setImageResource(R.drawable.baseline_call_made_24);
+            holder.friendButton.setEnabled(false);
+        } else {
+            holder.friendButton.setImageResource(R.drawable.baseline_add_circle_24);
+        }
+
+        if (user.getImage() != null) {
+            Uri uriImage = Uri.parse(user.getImage());
+            if (uriImage != null) {
+                String scheme = uriImage.getScheme();
+                if (scheme != null && scheme.equals("data")) {
+                    Bitmap bitmap = Helper.decodeImageFromBase64(uriImage);
+                    holder.userImage.setImageBitmap(bitmap);
+                } else {
+                    holder.userImage.setImageURI(uriImage);
+                }
+            }
+        } else {
+            holder.userImage.setImageResource(R.drawable.a);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView userNameTextView;
+        CircleImageView userImage;
+        ImageButton friendButton;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            userNameTextView = itemView.findViewById(R.id.userNameTextView);
+            friendButton = itemView.findViewById(R.id.friendButton);
+            friendButton.setOnClickListener(this);
+            userImage = itemView.findViewById(R.id.user_photo);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (listener != null) {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    listener.onItemClick(position);
+                }
+            }
+        }
+    }
+}
